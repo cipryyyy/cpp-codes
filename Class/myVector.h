dc2474bc253b2bf639380f583cd01037c13502a9
Andrea Cipriani <63643557+cipryyyy@users.noreply.github.com>
2024-11-06 23:35:01 +0100

    Update myVector.h

--- Class/myVector.h
+++ Class/myVector.h
@@ -1,126 +1,145 @@
-//
-//  Vector.h
-//
-//  Created by andre on 05/11/24.
-//
-
 #ifndef Vector_h
 #define Vector_h
 
+template <typename T>
 class Vector {
-    
-    int sz;         //Dimensione
-    double *elem;   //puntatore
-    
+    int sz;
+    int capacity;
+    T* elem;
+
 public:
-    class Invalid{};     //Lancio di errori
-    //Costruttore con elementi
-    Vector(std::initializer_list<double>lst) : sz{static_cast<int>(lst.size())}, elem{new double[sz]} {
-        std::copy(lst.begin(), lst.end(), elem);
-    }
-    //costruttore vuoto
-    explicit Vector(int size = 0) : sz{size}, elem{new double[sz]}
-    {
-        if (sz == 0) {
-            elem = nullptr;
+    class Invalid{};
+    
+    Vector(std::initializer_list<T> lst)
+        : sz{static_cast<int>(lst.size())}, capacity{static_cast<int>(lst.size())}, elem{new T[capacity]} {
+        for (int i = 0; i < sz; ++i) {
+            elem[i] = *(lst.begin() + i);
         }
+    }
+    
+    explicit Vector(int size = 0)
+        : sz{size}, capacity{size > 0 ? size : 1}, elem{new T[capacity]} {
         for (int i = 0; i < sz; i++) {
-            elem[i] = 0;                    //Assegnazione valori di default
+            elem[i] = T{};
         }
     }
-    //Costruttore di copia
-    Vector(const Vector& vec) : sz{vec.size()}, elem{new double[sz]}
-    {
-        std::copy(vec.elem, vec.elem+sz, elem);
+    
+    Vector(const Vector& vec)
+        : sz{vec.sz}, capacity{vec.capacity}, elem{new T[capacity]} {
+        for (int i = 0; i < sz; i++) {
+            elem[i] = vec.elem[i];
+        }
     }
-    //Distruttore
+    
     ~Vector() {
         delete[] elem;
     }
     
-    //move
-    Vector(Vector&& vec);
-    Vector& operator=(Vector&& vec);
-        
-    double& operator[](int index) {     //Passaggio a riferimento, consente lettura e scrittura
-        if (index < 0 || index >= sz) {     //Errori nell'utilizzo
-            throw Invalid();
-        }
-        return elem[index];
+    Vector(Vector&& vec) noexcept : sz{vec.sz}, capacity{vec.capacity}, elem{vec.elem} {
+        vec.sz = 0;
+        vec.capacity = 0;
+        vec.elem = nullptr;
     }
-    const double& operator[](int index) const { //Versione const
+    
+    Vector& operator=(Vector&& vec) noexcept {
+        if (this != &vec) {
+            delete[] elem;
+            elem = vec.elem;
+            sz = vec.sz;
+            capacity = vec.capacity;
+            vec.elem = nullptr;
+            vec.sz = 0;
+            vec.capacity = 0;
+        }
+        return *this;
+    }
+    
+    T& operator[](int index) {
         if (index < 0 || index >= sz) {
             throw Invalid();
         }
         return elem[index];
     }
-    double get(int index) {
+    
+    const T& operator[](int index) const {
         if (index < 0 || index >= sz) {
             throw Invalid();
         }
         return elem[index];
     }
-    void set(int index, double val = 0) {
+    
+    T get(int index) const {
+        if (index < 0 || index >= sz) {
+            throw Invalid();
+        }
+        return elem[index];
+    }
+    
+    void set(int index, T val = T{}) {
         if (index < 0 || index >= sz) {
             throw Invalid();
         }
         elem[index] = val;
     }
         
-    int size() const{       //Ritorna la dimensione
+    int size() const {
         return sz;
     }
     
-    //override =
-    Vector& operator= (const Vector& vec);
-    friend std::ostream& operator<<(std::ostream& os, const Vector& vec);     //Overload ostream
+    int get_capacity() const {
+        return capacity;
+    }
 
-    void pushback(const double& el) {       //Aggiunta di elementi
-        if (sz == 0) {
-            elem = new double[1];
+    Vector& operator=(const Vector& vec) {
+        if (this != &vec) {
+            T* p = new T[vec.capacity];
+            for (int i = 0; i < vec.sz; i++) {
+                p[i] = vec.elem[i];
+            }
+            delete[] elem;
+            elem = p;
+            sz = vec.sz;
+            capacity = vec.capacity;
+        }
+        return *this;
+    }
+    
+    friend std::ostream& operator<<(std::ostream& os, const Vector& vec) {
+        os << "[";
+        for (int i = 0; i < vec.sz; i++) {
+            os << vec[i];
+            if (i < vec.sz - 1) {
+                os << ", ";
+            }
+        }
+        os << "]";
+        return os;
+    }
+    
+    void pushback(const T& el) {
+        if (sz == capacity) {
+            capacity = capacity * 2;
+            T* new_elem = new T[capacity];
+            for (int i = 0; i < sz; i++) {
+                new_elem[i] = elem[i];
+            }
+            delete[] elem;
+            elem = new_elem;
         }
         elem[sz] = el;
         sz++;
     }
-};
-
-Vector& Vector::operator= (const Vector& vec) {
-    double *p = new double[vec.size()];
-    std::copy(vec.elem, vec.elem+sz, p);
-    delete[] elem;
-    elem = p;
-    sz = vec.sz;
-    return *this;
-}
-
-std::ostream& operator<<(std::ostream& os, const Vector& vec) {
-    os << "[";
-    for (int i = 0; i < vec.sz; i++) {
-        os << vec[i];
-        if (i < vec.size()-1) {
-            os << ", ";
+    
+    std::string join(std::string inter) {
+        std::string res;
+        for (int i = 0; i < sz; i++) {
+            res += elem[i];
+            if (i < sz-1) {
+                res += inter;
+            }
         }
+        return res;
     }
-    os << "]";
-    return os;
-}
-
-//move constructor
-Vector::Vector(Vector&& vec): sz{vec.sz}, elem{vec.elem}    //copio
-{
-    vec.sz = 0;             //annullo il vettore di partenza
-    vec.elem = nullptr;
-}
-// move assignment
-
-Vector& Vector::operator= (Vector&& vec) {
-    delete[] elem;      //Dealloco gli elementi attuali
-    elem = vec.elem;     //Punto a quelli nuovi
-    sz = vec.sz;        //copio la dimensione
-
-    vec.elem = nullptr;     //elimino la sorgente
-    vec.sz = 0;
-    return *this;       //ritorno
-}
+};
 
 #endif /* Vector_h */
